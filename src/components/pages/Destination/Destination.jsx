380283ea5ad10cfba212c8207f82799614f3e536
zivi <pereirazivencs232417@gmail.com>
2024-10-23 22:22:58 +0530

    update

--- src/components/pages/Destination/Destination.jsx
+++ src/components/pages/Destination/Destination.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Breadcrumb } from '../../Breadcrumbs/Breadcrumbs';
-import { Col,Container,Row } from 'react-bootstrap';
-import { destinationsData } from '../../utils/data';
-import Cards from '../../components/Cards/Cards'
+import React from "react";
+import { Breadcrumbs } from "../../Breadcrumbs/Breadcrumbs";
+import { Col, Container, Row } from "react-bootstrap";
+import { destinationsData } from "../../utils/data";
+import Cards from "../../Cards/Cards";
 const Destination = () => {
   return (
     <>
-        <Breadcrumbs title='Destination' pagename="Destinations" />
-        <section className='py-5'>
-          <Container>
-            <Row>
+      <Breadcrumbs title="Destination" pagename="Destinations" />
+      <section className="py-5">
+        <Container>
+          <Row>
             {destinationsData.map((destination, inx) => {
-                  return (
-                    <Col md="3" sm="6" key={inx} className='pb-4'>
-                    <Cards destination={destination} key={inx} />
-                    </Col>
-                  );
-                })}
-            </Row>
-          </Container>
-        </section>
+              return (
+                <Col md="3" sm="6" key={inx} className="pb-4">
+                  <Cards destination={destination} key={inx} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Destination
+export default Destination;
